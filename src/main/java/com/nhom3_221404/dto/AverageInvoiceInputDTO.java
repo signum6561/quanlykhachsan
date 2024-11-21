0c416c8ce435ece1298efa56dd5876e61d8e9c22
NDNguyen47 <ngductuan472004@gmail.com>
2024-11-21 11:36:04 +0700

    done

--- src/main/java/com/nhom3_221404/dto/AverageInvoiceInputDTO.java
+++ src/main/java/com/nhom3_221404/dto/AverageInvoiceInputDTO.java
@@ -1,5 +1,15 @@
 package com.nhom3_221404.dto;
 
+import java.time.Month;
+
 public class AverageInvoiceInputDTO {
-    
+    private Month month;
+
+    public AverageInvoiceInputDTO(Month month) {
+        this.month = month;
+    }
+
+    public Month getMonth() {
+        return month;
+    }
 }
--- src/main/java/com/nhom3_221404/usecase/AverageInvoice/AverageInvoiceDatabaseBoundary.java
+++ src/main/java/com/nhom3_221404/usecase/AverageInvoice/AverageInvoiceDatabaseBoundary.java
@@ -4,5 +4,5 @@ import java.util.List;
 import com.nhom3_221404.entity.Invoice;
 
 public interface AverageInvoiceDatabaseBoundary {
-    List<Invoice> getAllInvoices();
+    List<Invoice> getInvoicesByMonth(int month);
 }
